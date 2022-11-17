9300a2ffdc7b4be97926aaf6e6c8feed8a649cdf
Francisco <francisco.nasich@mercadolibre.com>
2022-11-17 13:55:47 -0300

    Initial Commit

---
+++ Streamix/StreamixApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamixApp.swift
+//  Streamix
+//
+//  Created by Francisco Tomas Nasich on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct StreamixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
